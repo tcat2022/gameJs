5341e1ba484e3c3b6e627e0c48652813c94237d8
tommy <tcatty7@gmail.com>
2022-09-08 13:15:46 -0400

    fr

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ document.addEventListener('mousemove', function(e){
     x = e.clientX
     y = e.clientY
     y= -y;
+    x= -x;
     world.style.transform = "translateZ(600px) rotateX("+y+"deg) rotateY("+x+"deg)"
 })
